9b15926e1139e04cf0511cacc7863c1e33e6dda5
Sumit Tamrakar <tamrakarSumit7@gmail.com>
2020-03-01 22:28:53 -0700

    Adding first endpoint

---
+++ CityInfo.API/Controllers/CitiesController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CityInfo.API.Controllers
+{
+    [ApiController]
+    [Route ("api/cities")]
+    public class CitiesController : ControllerBase
+    {
+        [HttpGet]
+        public JsonResult GetCities()
+        {
+            return new JsonResult(
+                new List<object>()
+                {
+                    new { id = 1, Name = "New York City"},
+                    new { id = 2, Name = "Antwerp"}
+                });
+        }
+    }
+}
